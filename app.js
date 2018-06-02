ad7b895993456dffd74c11a32df192363c9e0a3d
Bhupender Singh <bhupenders225@gmail.com>
2018-06-02 23:06:20 +0530

    updating bootstrap and app.js and including header and nav ejs files

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ var app = express();
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res) {
-    res.send("Welcome to the YelpCamp");
+    res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res) {
+    
+    var campgrounds = [
+        {name:"Salmon Creek", image: "https://farm4.staticflickr.com/3297/3518227895_339a010a78.jpg"},
+        {name:"Granite Hill", image: "https://farm4.staticflickr.com/3457/3871440451_181099e545.jpg"},
+        {name:"Mountain Goat's rest", image: "https://farm4.staticflickr.com/3060/3021047498_eaab93a32b.jpg"},
+    ];
+
+    res.render("campgrounds", {campgrounds:campgrounds});
 });
 
 app.listen(3000, function() {
