4bd00cc5df4fec10afd4041d8174ed0d86b7caaf
Bhupender Singh <bhupenders225@gmail.com>
2018-06-05 23:56:13 +0530

    updating basic files

--- app.js
+++ app.js
@@ -1,84 +1,80 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+var express = require("express"),
+    app = express();
+bodyParser = require("body-parser"),
+    mongoose = require("mongoose")
 
-var app = express();
-
-
-//  Temporary Database as array..
-var campgrounds = [
-        {
-            name: "Salmon Creek",
-            image: "https://farm4.staticflickr.com/3297/3518227895_339a010a78.jpg"
-        }, 
-        {
-            name: "Granite Hill",
-            image: "https://farm4.staticflickr.com/3457/3871440451_181099e545.jpg"
-        }, 
-        {
-            name: "Mountain Goat's rest",
-            image: "https://farm4.staticflickr.com/3060/3021047498_eaab93a32b.jpg"
-        },
-        {
-            name: "Salmon Creek",
-            image: "https://farm4.staticflickr.com/3297/3518227895_339a010a78.jpg"
-        }, 
-        {
-            name: "Granite Hill",
-            image: "https://farm4.staticflickr.com/3457/3871440451_181099e545.jpg"
-        }, 
-        {
-            name: "Mountain Goat's rest",
-            image: "https://farm4.staticflickr.com/3060/3021047498_eaab93a32b.jpg"
-        },
-        {
-            name: "Salmon Creek",
-            image: "https://farm4.staticflickr.com/3297/3518227895_339a010a78.jpg"
-        }, 
-        {
-            name: "Granite Hill",
-            image: "https://farm4.staticflickr.com/3457/3871440451_181099e545.jpg"
-        }, 
-        {
-            name: "Mountain Goat's rest",
-            image: "https://farm4.staticflickr.com/3060/3021047498_eaab93a32b.jpg"
-        }, 
-        {
-            name: "Mountain Goat's rest",
-            image: "https://farm4.staticflickr.com/3060/3021047498_eaab93a32b.jpg"
-        }
-];
-
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
 app.set("view engine", "ejs");
 
-app.get("/", function (req, res) {
-    res.render("landing");
+//  Schema setup
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String,
+    description: String
 });
 
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+//  adding a campground Data into database
+
+campground.create({
+        name: "Salmon Creek",
+        image: "https://farm4.staticflickr.com/3297/3518227895_339a010a78.jpg"
+    },
+    function (err, addedData) {
+        if (err) {
+            console.log("Something went wrong while CREATING data \n", err);
+        } else {
+            console.log("SUCCESS on CREATING \n", addedData);
+        }
+    });
+
+
+
+
+
+// app.get("/", function (req, res) {
+//     res.render("landing");
+// });
+
+// Index route -- to show all campground
 app.get("/campgrounds", function (req, res) {
-    res.render("campgrounds", {
-        campgrounds: campgrounds
+
+    Campground.find({}, function (err, allCampgrounds) {
+        if (err) {
+            console.log("Something Went Wrong ! \n", err);
+        } else {
+            res.render("campgrounds", {
+                campgrounds: allCampgrounds
+            });
+        }
     });
 });
 
-
+// Create route -- add new campground into database
 app.post("/campgrounds", function (req, res) {
     // res.send("You are on POST page !");
     var name = req.body.name;
     var image = req.body.image;
-    var newCampGround = {
+    var newCampground = {
         name: name,
         image: image
     };
 
-    campgrounds.push(newCampGround);
-    res.redirect("/campgrounds");
+    Campground.create(newCampground, function (err, newAddedCampground) {
+        if (err) {
+            console.log("Something went wrong while adding new Campgound in DB : \n", err);
+        } else {
+            res.redirect("/campgrounds");
+        }
+    });
 });
 
-
+// New route  --show form to create new campground
 app.get("/campgrounds/new", function (req, res) {
     res.render("new");
 });
@@ -86,3 +82,9 @@ app.get("/campgrounds/new", function (req, res) {
 app.listen(3000, function () {
     console.log("Yelpcamp has been started !");
 });
+
+
+
+//  mongo db starting code
+
+//   "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe" --dbpath "C:\data\db"
